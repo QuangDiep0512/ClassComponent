cac1a5d7692350858c5ef3ee6e2d144a42c9045a
QuangDiep0512 <diepsang0512@gmail.com>
2023-02-17 16:47:44 +0700

    video4 dynamic in template

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let src = "https://www.youtube.com/watch?v=yOoUyj-3XK0&list=RDyOoUyj-3XK0&start_radio=1";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Lê Quang Điệp</h1>
+        <a href={src} target="_blank">Welcom to youtube</a>
       </header>
     </div>
   );
